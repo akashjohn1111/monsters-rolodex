9245062aa5d69be2ea946fa5f2924347675d4ca5
Akash <akash@Akashs-MacBook-Pro.local>
2020-09-10 20:26:22 +0530

    Updates

---
+++ precache-manifest.c03bac5d5a474a3922b1050e34b3b1bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ca76cb79a8b2d7e61b40abfabe7d59",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2938e5346f3d8a59c375",
+    "url": "/monsters-rolodex/static/css/main.d964988b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2938e5346f3d8a59c375",
+    "url": "/monsters-rolodex/static/js/main.7b9a987c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
